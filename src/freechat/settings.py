e6b4b2721efa67226f337d5c90eed18522347709
daliseiy <obakunleoluseye@gmail.com>
2020-07-15 15:32:30 +0100

    update

--- src/freechat/settings.py
+++ src/freechat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,4 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
+
+django_heroku.settings(locals())
