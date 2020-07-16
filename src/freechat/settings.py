0c1a54d012847696477db77dfff53fd229fadb8a
daliseiy <obakunleoluseye@gmail.com>
2020-07-16 13:15:13 +0100

    redis fix

--- src/freechat/settings.py
+++ src/freechat/settings.py
@@ -79,7 +79,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": os.environ.get("REDIS_URL")
+            "hosts": [os.environ.get("REDIS_URL")]
         },
     },
 }
